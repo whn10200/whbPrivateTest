588adaea0c6452271480a2bfd467184b7f9b6faf
王华北 <wanghuabei@51tuodao.com>
2018-02-05 10:01:21 +0800

    添加SSM框架

---
+++ whbPrivateSsm/src/main/java/com/whb/ssm/App.java
@@ -0,0 +1,13 @@
+package com.whb.ssm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
