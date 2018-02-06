a81e0c9809055429a920f75a3b14a688062efe74
王华北 <wanghuabei@51tuodao.com>
2018-02-06 15:51:35 +0800

    333

--- src/main/java/com/whb/utils/IpUtil.java
+++ src/main/java/com/whb/utils/IpUtil.java
@@ -7,7 +7,7 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
-
+ 
 public class IpUtil {
 	public static String getIp() {
 		String localip = null;
